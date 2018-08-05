6b165b7c4e7255fecbb9ad3eaa950a87106ee72c
mottox2 <y.mottox2@gmail.com>
2018-08-05 23:23:49 +0900

    Add sample to capture page

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,9 +1,28 @@
 import * as React from 'react'
 import { hot } from 'react-hot-loader'
+import { NativeImage, Rectangle, WebviewTag } from 'electron'
 
-class App extends React.Component {
-  capture() {
-    console.log('capture')
+interface Props {} 
+
+class App extends React.Component<Props> {
+  webview: {
+    current: WebviewTag
+  }
+
+  constructor(props: Props) {
+    super(props)
+    this.webview = React.createRef()
+  }
+
+  capture = () => {
+    console.log(this.webview)
+    const rect : Rectangle = {x: 200, y: 200, width: 100, height: 100}
+    this.webview.current.capturePage(
+      rect,
+      (image: NativeImage) => {
+        console.log(image.toDataURL())
+      }
+    )
   }
 
   render() {
@@ -11,7 +30,7 @@ class App extends React.Component {
       <div>
         <button onClick={this.capture}>Capture...</button>
         <p>Hello World</p>
-        <webview src="https://github.com" />
+        <webview ref={this.webview} src="https://github.com" />
       </div>
     )
   }
