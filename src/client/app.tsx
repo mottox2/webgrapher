641937ef4fb27c26d718d42113263c0e3b203488
mottox2 <y.mottox2@gmail.com>
2018-08-05 23:46:32 +0900

    Add ability to edit capture spot

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -2,11 +2,21 @@ import * as React from 'react'
 import { hot } from 'react-hot-loader'
 import { NativeImage, Rectangle, WebviewTag } from 'electron'
 
-interface Props {} 
+interface Props {}
 
 class App extends React.Component<Props> {
   webview: {
     current: WebviewTag
+  }
+
+  state = {
+    captureDataURL: '',
+    rect: {
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100
+    }
   }
 
   constructor(props: Props) {
@@ -16,21 +26,62 @@ class App extends React.Component<Props> {
 
   capture = () => {
     console.log(this.webview)
-    const rect : Rectangle = {x: 200, y: 200, width: 100, height: 100}
-    this.webview.current.capturePage(
-      rect,
-      (image: NativeImage) => {
-        console.log(image.toDataURL())
-      }
-    )
+    const rect: Rectangle = this.state.rect
+    console.log(rect)
+    this.webview.current.capturePage(rect, (image: NativeImage) => {
+      this.setState({ captureDataURL: image.toDataURL() })
+      console.log(image.toDataURL())
+    })
   }
 
   render() {
+    const { rect, captureDataURL } = this.state
     return (
       <div>
-        <button onClick={this.capture}>Capture...</button>
-        <p>Hello World</p>
         <webview ref={this.webview} src="https://github.com" />
+        <form onSubmit={this.capture}>
+          <input
+            value={rect.x}
+            type="number"
+            onChange={e =>
+              this.setState({ rect: { ...rect, x: Number(e.target.value) } })
+            }
+          />
+          <input
+            value={rect.y}
+            type="number"
+            onChange={e =>
+              this.setState({ rect: { ...rect, y: Number(e.target.value) } })
+            }
+          />
+          <input
+            value={rect.width}
+            type="number"
+            onChange={e =>
+              this.setState({
+                rect: { ...rect, width: Number(e.target.value) }
+              })
+            }
+          />
+          <input
+            value={rect.height}
+            type="number"
+            onChange={e =>
+              this.setState({
+                rect: { ...rect, height: Number(e.target.value) }
+              })
+            }
+          />
+          <button type="submit">Capture</button>
+        </form>
+        <div>
+          {captureDataURL && (
+            <img
+              style={{ transform: 'scale(0.5)', transformOrigin: 'left top' }}
+              src={captureDataURL}
+            />
+          )}
+        </div>
       </div>
     )
   }
